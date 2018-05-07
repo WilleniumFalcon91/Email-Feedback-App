f2b3c220a51f1c4e0495fec2e7cbd2f34bc4b394
William Elliott <wcelliott1991@gmail.com>
2018-05-06 20:34:47 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ App: 'Will\'s App'});
 });
 
 const PORT = process.env.PORT || 5000;
