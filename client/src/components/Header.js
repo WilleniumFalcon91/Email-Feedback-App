d5e577a788e39f4d2c09a62c9b7d6d395ea8997c
William Elliott <wcelliott1991@gmail.com>
2018-06-30 09:34:33 -0400

    initial styling of header and root application

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper light-blue">
+          <a href="#!" className="left brand-logo">
+            Emaily
+            <i className="material-icons">email</i>
+          </a>
+          <ul className="right">
+            <li>
+              <a>Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
