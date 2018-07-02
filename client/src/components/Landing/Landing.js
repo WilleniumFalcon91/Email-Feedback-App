74918a072bffb4202a78aa6ea48666ae5cf4d9a6
William Elliott <wcelliott1991@gmail.com>
2018-07-01 21:38:44 -0400

    creates landing component

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./landing.css";
+
+const Landing = () => {
+  return (
+    <section>
+      <div>
+        <h1>Emaily!</h1>
+        Collect feedback from your users
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
